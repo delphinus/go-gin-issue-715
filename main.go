98d541852135f6b48536588d91baa06856fa54fc
delphinus <me@delphinus.dev>
2020-06-04 18:59:54 +0900

    Add tests

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type CreateParams struct {
+	Username     string `json:"username" validate:"required"`
+	Guests       Guests `json:"guests"`
+	RoomType     string `json:"roomType" validate:"required"`
+	CheckinDate  string `json:"checkinDate"`
+	CheckoutDate string `json:"checkoutDate"`
+}
+
+type Guests struct {
+	Person []Person `json:"person"`
+}
+
+type Person struct {
+	Firstname string `json:"firstname" validate:"required"`
+	Lastname  string `json:"lastname"`
+}
+
+func main() {
+	v := CreateParams{
+		Guests: Guests{
+			Person: []Person{
+				{Firstname: "foo", Lastname: "bar"},
+				{Lastname: "bar"},
+			},
+		},
+	}
+	validate := validator.New()
+	if err := validate.Struct(v); err != nil {
+		fmt.Printf("validation1:\n%+v\n\n", err)
+	}
+
+	for _, p := range v.Guests.Person {
+		if err := validate.Struct(p); err != nil {
+			fmt.Printf("validation2\n:%+v\n\n", err)
+		}
+	}
+}
